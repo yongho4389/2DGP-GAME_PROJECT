4a797f772e045230133f80a3623f57b4c4db8bd4
yongho4389 <program0722@naver.com>
2025-10-08 22:23:17 +0900

    대략적인 캐릭터 애니메이션 구현

--- character_animation_test.py
+++ character_animation_test.py
@@ -2,13 +2,16 @@ from pico2d import *
 
 open_canvas()
 
-character = load_image('character_running.png')
+character = load_image('character_motion_sheets.png')
 
 frame = 0
+motion = 0
 while True:
     clear_canvas()
-    character.clip_draw(frame * 143, 0, 143, 166, 400, 300)
+    # 1040 x 1488
+    character.clip_draw(frame * (1040 // 7), (motion * 1488 // 8), 1040 // 7, 1488 // 8, 400, 300)
     frame = (frame + 1) % 7
+    if frame == 0: motion += 1
     update_canvas()
     delay(0.1)
 
