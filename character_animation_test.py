44f049a68a127579c35f3a1e63fa01dff80735be
yongho4389 <program0722@naver.com>
2025-10-08 23:12:58 +0900

    스프라이트 시트 조정으로 애니메이션 자연스럽게 변경

--- character_animation_test.py
+++ character_animation_test.py
@@ -5,14 +5,13 @@ open_canvas()
 character = load_image('character_motion_sheets.png')
 
 frame = 0
-motion = 0
+motion = 4
 while True:
     clear_canvas()
-    # 1040 x 1488
-    character.clip_draw(frame * (1040 // 7), (motion * 1488 // 8), 1040 // 7, 1488 // 8, 400, 300)
-    frame = (frame + 1) % 7
-    if frame == 0: motion += 1
+    # 1288 x 930
+    character.clip_draw(frame * (1288 // 8), (motion * 930 // 5), 1288 // 8, 930 // 5, 400, 300)
+    frame = (frame + 1) % 8
     update_canvas()
-    delay(0.1)
+    delay(0.2)
 
 close_canvas()
