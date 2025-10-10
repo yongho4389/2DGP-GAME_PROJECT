5066b677a0f98ab15da16973f4c04f71047c0751
yongho4389 <program0722@naver.com>
2025-10-10 21:46:44 +0900

    불필요한 코드 제거

--- Class_character_controls.py
+++ Class_character_controls.py
@@ -22,7 +22,6 @@ def Character_events():
             character.change_direction_right()
             character.draw_running()
         elif event.type == SDL_KEYUP and (event.key == SDLK_a or event.key == SDLK_d):
-            character.motion = 0
             character.draw_stand()
         # 점프 및 착지
         elif event.type == SDL_KEYDOWN and event.key == SDLK_k:
