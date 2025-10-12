01f37a9f47096bde99ae490d884b05b794564f35
yongho4389 <program0722@naver.com>
2025-10-12 21:24:34 +0900

    보스 스테이지 출력되도록 스프라이트 시트 추가 및 출력

--- Class_stage.py
+++ Class_stage.py
@@ -8,21 +8,51 @@ class Normal_Stage:
         self.width = 800
         self.height = 600
         self.w = 3084
-        self.h = 1872
+        self.h = 2496
         self.x = 400
         self.y = 400
-        self.stage_level = 0
+        self.stage_level = 3
+        self.special_stage = True
 
     def draw(self):
-        # 카메라 위치 최신화
-        camera.start_position = 0
-        camera.end_position = (self.width * 3) - 40
-        for i in range(3):
+        if self.special_stage: # 특수 스테이지인 경우 맵 크기는 1배
+            # 카메라 위치 최신화
+            camera.start_position = 0
+            camera.end_position = self.width
             # 구름
-            self.image.clip_draw(0 * self.w // 3, (2 - self.stage_level) * self.h // 3, self.w // 3, self.h // 3, (i * (self.width - 20)) + self.x - camera.x, self.y, self.width, 1000)
+            self.image.clip_draw(0 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                 self.w // 3, self.h // 4,
+                                 self.x, self.y,
+                                 self.width, 1000)
             # 나무
-            self.image.clip_draw(1 * self.w // 3, (2 - self.stage_level) * self.h // 3, self.w // 3, self.h // 3, (i * (self.width - 20)) + self.x - camera.x, self.y, self.width, 1000)
+            self.image.clip_draw(1 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                 self.w // 3, self.h // 4,
+                                 self.x, self.y,
+                                 self.width, 1000)
             # 땅
-            self.image.clip_draw(2 * self.w // 3, (2 - self.stage_level) * self.h // 3, self.w // 3, self.h // 3, (i * (self.width - 20)) + self.x - camera.x, self.y // 4, self.width, 400)
+            self.image.clip_draw(2 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                 self.w // 3, self.h // 4,
+                                 self.x, self.y // 4,
+                                 self.width, 400)
+        else:
+            # 카메라 위치 최신화
+            camera.start_position = 0
+            camera.end_position = (self.width * 3) - 40
+            for i in range(3):
+                # 구름
+                self.image.clip_draw(0 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                     self.w // 3, self.h // 4,
+                                     (i * (self.width - 20)) + self.x - camera.x, self.y,
+                                     self.width, 1000)
+                # 나무
+                self.image.clip_draw(1 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                     self.w // 3, self.h // 4,
+                                     (i * (self.width - 20)) + self.x - camera.x, self.y,
+                                     self.width, 1000)
+                # 땅
+                self.image.clip_draw(2 * self.w // 3, (3 - self.stage_level) * self.h // 4,
+                                     self.w // 3, self.h // 4,
+                                     (i * (self.width - 20)) + self.x - camera.x, self.y // 4,
+                                     self.width, 400)
 
 stage = Normal_Stage()
