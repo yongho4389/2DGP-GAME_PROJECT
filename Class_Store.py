78a80c09f7ec090a6b13f6c59233e5c6fbd1c197
yongho4389 <program0722@naver.com>
2025-10-26 15:27:51 +0900

    X버튼 구현

--- Class_Store.py
+++ Class_Store.py
@@ -9,6 +9,13 @@ class Store:
         self.x = self.stage.width // 2 + 100
         self.y = self.stage.height // 2 - 75
 
+        # 상점 기본창 위치
+        self.window_x = self.stage.width // 2
+        self.window_y = self.stage.height // 2
+        # X버튼 위치
+        self.x_button_x = self.window_x + 350
+        self.x_button_y = self.window_y + 750
+
         self.store_onoff = False
 
     def draw(self):
@@ -16,8 +23,15 @@ class Store:
     def store_draw(self):
         if self.store_onoff:
             # 기본 창 형성
-            self.element_image.clip_draw(0, 170, 210, 170, self.stage.width // 2, self.stage.height // 2, 1400, 600)
+            self.element_image.clip_draw(0, 170, 210, 170, self.window_x, self.window_y, 1400, 600)
+            # X 버튼
+            self.element_image.clip_draw(210, 170, 210, 170, self.x_button_x, self.x_button_y // 2, 100, 100)
 
     def store_click(self, mx, my):
-        if self.stage.special_stage and mx >= self.x - 150 and mx <= self.x + 150 and my >= self.y - 250 and my <= self.y + 250:
+        if self.stage.special_stage and not self.store_onoff and mx >= self.x - 150 and mx <= self.x + 150 and my >= self.y - 250 and my <= self.y + 250:
             self.store_onoff = True
+            return
+        if self.store_onoff:
+            if mx >= self.x_button_x - 25 and mx <= self.x_button_x + 25 and my >= (self.x_button_y // 2) - 25 and my <= (self.x_button_y // 2) + 25:
+                self.store_onoff = False
+                return
