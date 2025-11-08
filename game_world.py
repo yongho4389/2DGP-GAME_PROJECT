59b40cd3b04c23e9fb08183318282750b0bd9725
yongho4389 <program0722@naver.com>
2025-11-08 22:47:13 +0900

    충돌 처리 준비

--- game_world.py
+++ game_world.py
@@ -23,3 +23,35 @@ def render():
 def clear():
     for layer in world:
         layer.clear()
+
+def collide(a, b):
+    # 인자로 전달한 객체의 바운딩 박스를 구한다
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+    # 충돌이 일어나지 않으면 False 리턴, 충돌이 일어나면 맨 마지막에 True를 리턴 (아래 if문들은 충돌이 절대로 일어나지 않을 경우에 대한 처리를 하는 것)
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    return True
+
+collision_pairs = {}
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs: # 처음 추가되는 그룹인 경우
+        collision_pairs[group] = ([], []) # 해당 그룹을 위한 빈 리스트를 작성 (a가 왼쪽, b가 오른쪽에 들어감) (None을 인자로 받은 것은 해당 부분의 리스트를 채우지 않겠다는 뜻
+    if a: # a가 None이 아닌 경우에만 추가
+        collision_pairs[group][0].append(a)
+    if b: # b가 None이 아닌 경우에만 추가
+        collision_pairs[group][1].append(b)
+    return None
+
+
+def handle_collision():
+    for group, pairs in collision_pairs.items(): # collision_pairs 딕셔너리의 (키, 값) 쌍을 하나씩 꺼내서 처리 (pairs는 (a 리스트, b 리스트) 튜플)
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b) # a에게는 누구와 충돌했는지를 알려주고, 그 group도 알려주어 a가 스스로 b에 대한 처리를 하도록 만든다.
+                    b.handle_collision(group, a) # b에게도 동일하게 처리
+
+    return None
